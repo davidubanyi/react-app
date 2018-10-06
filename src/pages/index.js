3fbd806e27fb7888d0e6471ebf6c93923280db6a
davidubanyi <ubanyi@gmail.com>
2018-10-06 22:42:38 +0100

    built the hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,17 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design and code react apps</h1>
-    <p>
-      Complete courses about the best tools and design systems. Prototype and
-      build apps with React and Swift.
-    </p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Learn to design and code react apps</h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift.
+        </p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
